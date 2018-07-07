82323d0cb3f803da44b676f5e58cdf722d5ac662
Thomas Genin <xt6.thomas.genin@gmail.com>
2018-07-06 23:21:31 -0700

    Dispaly number of keys + nicer formatting of the tables

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const updateNotifier = require("update-notifier");
 const pkg = require("./package.json");
+const thousands = require("thousands");
 
 updateNotifier({ pkg }).notify();
 
@@ -20,6 +21,33 @@ if (!fs.existsSync(program.path)) {
   throw new Error(`File '${program.path}' does not exists.`);
 }
 
+const tableConfig = {
+  // chars: {
+  //   top: "",
+  //   "top-mid": "",
+  //   "top-left": "",
+  //   "top-right": "",
+  //   bottom: "",
+  //   "bottom-mid": "",
+  //   "bottom-left": "",
+  //   "bottom-right": "",
+  //   left: "+",
+  //   "left-mid": "",
+  //   mid: "-",
+  //   "mid-mid": "",
+  //   right: "",
+  //   "right-mid": "",
+  //   middle: "|"
+  // },
+  colAligns: ["left", "right", "right", "right"],
+  style: {
+    "padding-left": 0,
+    "padding-right": 2,
+    head: ["yellow"]
+  },
+  head: ["Key Name", "Count", "Total Size", "% of Total"]
+};
+
 console.log("Processing", program.path);
 const raw = fs.readFileSync(program.path, { encoding: "utf-8" });
 const obj = JSON.parse(raw);
@@ -28,68 +56,76 @@ const documentStats = new Stats(raw.length);
 main(obj, documentStats, "");
 
 console.log();
-console.log("----------");
+console.log("======================");
 console.log("Keys");
-console.log("----------");
-console.log(documentStats.nbOfKey(), "keys");
+console.log("======================");
+console.log("Number of keys: " + thousands(documentStats.nbOfKey()));
 console.log(
   "Key size",
-  documentStats.keySize(),
+  thousands(documentStats.keySize()),
+  "-",
   documentStats.perc(documentStats.keySize()) + "%"
 );
 
 console.log();
+console.log();
 console.log("Most frequent keys");
+console.log("----------------------");
 // instantiate
-let table = new Table({
-  head: ["Key Name", "Count", "Total Size", "% of Total"],
-  style: {
-    head: ["yellow"]
-  }
-});
+let table = new Table(tableConfig);
 
 documentStats
   .keySortedByCount()
   .slice(-program.items)
   .forEach(k => {
     const size = k.length * documentStats.keys[k];
-    table.push([k, documentStats.keys[k], size, documentStats.perc(size)]);
+    table.push([
+      k,
+      thousands(documentStats.keys[k]),
+      thousands(size),
+      documentStats.perc(size)
+    ]);
   });
 
 console.log(table.toString());
 
 console.log();
+console.log();
 console.log("Heaviest keys");
-table = new Table({
-  head: ["Key Name", "Count", "Total Size", "% of Total"],
-  style: {
-    head: ["yellow"]
-  }
-});
+console.log("----------------------");
+table = new Table(tableConfig);
 
 documentStats
   .keySortedBySize()
   .slice(-program.items)
   .forEach(k => {
     const size = k.length * documentStats.keys[k];
-    table.push([k, documentStats.keys[k], size, documentStats.perc(size)]);
+    table.push([
+      k,
+      thousands(documentStats.keys[k]),
+      thousands(size),
+      documentStats.perc(size)
+    ]);
   });
 
 console.log(table.toString());
 
 console.log();
-console.log("----------");
+console.log("======================");
 console.log("Values");
-console.log("----------");
+console.log("======================");
 
 console.log();
 console.log("Most Frequent Values");
-table = new Table({
-  head: ["Values", "Count", "Total Size", "% of Total"],
-  style: {
-    head: ["yellow"]
-  }
-});
+console.log("----------------------");
+table = new Table(
+  Object.assign(
+    {
+      head: ["Values", "Count", "Total Size", "% of Total"]
+    },
+    tableConfig
+  )
+);
 
 documentStats
   .valuesSortedByCount()
@@ -98,21 +134,25 @@ documentStats
     const size = documentStats.values[k].length * k.length;
     table.push([
       k.length > 60 ? k.substr(0, 60) + "..." : k,
-      documentStats.values[k].length,
-      size,
+      thousands(documentStats.values[k].length),
+      thousands(size),
       documentStats.perc(size) + "%"
     ]);
   });
 console.log(table.toString());
 
 console.log();
+console.log();
 console.log("Heaviest Values");
-table = new Table({
-  head: ["Values", "Count", "Total Size", "% of Total"],
-  style: {
-    head: ["yellow"]
-  }
-});
+console.log("----------------------");
+table = new Table(
+  Object.assign(
+    {
+      head: ["Values", "Count", "Total Size", "% of Total"]
+    },
+    tableConfig
+  )
+);
 
 documentStats
   .valuesSortedBySize()
@@ -121,30 +161,33 @@ documentStats
     const size = documentStats.values[k].length * k.length;
     table.push([
       k.length > 60 ? k.substr(0, 60) + "..." : k,
-      documentStats.values[k].length,
-      size,
+      thousands(documentStats.values[k].length),
+      thousands(size),
       documentStats.perc(size) + "%"
     ]);
   });
 console.log(table.toString());
 
 console.log();
-console.log("----------");
-console.log("Duplicates Key/values");
-console.log("----------");
+console.log();
+console.log("======================");
+console.log("DUPLICATES KEY/VALUES");
+console.log("======================");
 
-table = new Table({
-  head: ["Value", "Count", "Total Size"],
-  style: {
-    head: ["yellow"]
-  }
-});
+table = new Table(
+  Object.assign(
+    {
+      head: ["Value", "Count", "Total Size"]
+    },
+    tableConfig
+  )
+);
 
 documentStats
   .dupsValue()
   .slice(-program.items)
   .forEach(k => {
-    table.push([k, documentStats.keyValue[k], k.length]);
+    table.push([k, documentStats.keyValue[k], k.length, 0]);
   });
 console.log(table.toString());
 
