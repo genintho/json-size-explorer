12b303a403d6acd73443785ab0b35738c53d5401
Thomas Genin <xt6.thomas.genin@gmail.com>
2019-04-21 21:25:19 -0700

    Process item in array to detect nested objects

--- src/JsonSizeExplorer/main.ts
+++ src/JsonSizeExplorer/main.ts
@@ -1,7 +1,7 @@
-import * as _ from "lodash"
-import {Stats, Indexable} from "./stats";
+import * as _ from "lodash";
+import { Stats, Indexable } from "./stats";
 
-function _main(obj:Indexable, stats: Stats, path:string) {
+function _main(obj: Indexable, stats: Stats, path: string) {
     const keys = Object.keys(obj);
 
     keys.forEach((key) => {
@@ -9,11 +9,16 @@ function _main(obj:Indexable, stats: Stats, path:string) {
         stats.addKeyValue(key, value, path);
         if (_.isPlainObject(value)) {
             _main(obj[key], stats, path + "." + key);
+        } else if (_.isArray(value)) {
+            value.forEach((item, idx) => {
+                if (_.isPlainObject(item))
+                    _main(item, stats, path + "." + key + "." + idx);
+            });
         }
     });
 }
 
-export default function main(raw:string) {
+export default function main(raw: string) {
     const stat = new Stats(raw.length);
     _main(JSON.parse(raw), stat, "");
     return stat;
